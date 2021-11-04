d4cf001f5decee801b5c8ccfad7db323e65f028a
Fatema Alam <alamtoma@gmail.com>
2021-11-04 21:19:33 +0100

    commite oblig2 file

--- leapYearTest_oblig2/src/test/java/no/hiof/fatemaala/oblig2/test/leapYearTest.java
+++ leapYearTest_oblig2/src/test/java/no/hiof/fatemaala/oblig2/test/leapYearTest.java
@@ -11,18 +11,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class leapYearTest {
 
     leapYear ly = new leapYear();
+
+    
     @Test
     public void year_is_divided_by_4_is_a_LeapYear(){
         assertEquals(true, Year.isLeap(2092));
     }
+
+
     @Test
     public void year_is_divided_by_400_is_a_LeapYear(){
         assertEquals(true, Year.isLeap(2000));
     }
+
+
     @Test
     public void year_isNot_divided_by_4_isNot_a_LeapYear(){
         assertEquals(false, Year.isLeap(1700));
     }
+
+
     @Test
     public void year_isNot_divided_by_400_but_divided_by_100_is_a_LeapYear(){
         assertEquals(false, Year.isLeap(1900));
